acdfbc5f96aa564f9c969d9a7ece722c1b550558
Tô Thanh Phong <thnhphong4869@gmail.com>
2026-03-02 08:33:46 +0700

    Fix FRONTEND_URL, FRONTEND_URL_2

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ dotenv.config();
 const app: Application = express();
 const PORT = env.port;
 const FRONTEND_URL = process.env.FRONTEND_URL;
-
+const FRONTEND_URL_2 = process.env.FRONTEND_URL_2;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
@@ -33,6 +33,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   FRONTEND_URL,
+  FRONTEND_URL_2
 ];
 
 app.use(cors({
